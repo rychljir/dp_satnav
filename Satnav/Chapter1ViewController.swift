f1ab10107de972632d0afe42e164a042efece9f9
Petr Mares <scientica@petr-mac-mini.local>
2017-03-24 18:59:33 +0100

    priprava na prvni tri ukoly, priprava na multilang texty

--- Satnav/Chapter1ViewController.swift
+++ Satnav/Chapter1ViewController.swift
@@ -16,7 +16,7 @@ class Chapter1ViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLoad() {
 
         slideScrollView.delegate = self
-        let slides:[Slide] = createSlide()
+        let slides:[UIView] = createSlide()
         setupSlideScrollView(slides: slides)
         
         pageControl.numberOfPages = slides.count
@@ -26,16 +26,25 @@ class Chapter1ViewController: UIViewController, UIScrollViewDelegate {
     }
 
 
-    func createSlide() -> [Slide]{
-        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
-        slide1.label.text = "Slide1"
+    func createSlide() -> [UIView]{
+        let slide1: TitleTextSlide = Bundle.main.loadNibNamed("TitleTextSlide", owner: self, options: nil)?.first as! TitleTextSlide
+        let title = NSLocalizedString("HELLO_WORLD",comment:"default")
+        slide1.initSlide(titleText: title, description: "Kdyz praveky clovek na lovu objevil mamuta.")
+        slide1.setBack(UIImage(named: "task01_1_bck")!)
         
-        let slide2:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
-        slide2.label.text = "Slide2"
-        return [slide1, slide2]
+        let slide2: TitleTextSlide = Bundle.main.loadNibNamed("TitleTextSlide", owner: self, options: nil)?.first as! TitleTextSlide
+        slide2.initSlide(titleText: "Princip urceni polohy", description: "Kdyz praveky clovek na lovu objevil mamuta.")
+        slide2.setBack(UIImage(named: "task01_2_bck")!)
+        
+        let slide3: TitleTextSlide = Bundle.main.loadNibNamed("TitleTextSlide", owner: self, options: nil)?.first as! TitleTextSlide
+        slide3.initSlide(titleText: "Princip urceni polohy", description: "Kdyz praveky clovek na lovu objevil mamuta.")
+        slide3.setBack(UIImage(named: "task01_3_bck")!)
+        slide3.setTextColor(UIColor.white)
+        
+        return [slide1, slide2, slide3]
     }
     
-    func setupSlideScrollView(slides:[Slide]){
+    func setupSlideScrollView(slides:[UIView]){
         slideScrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
         //slideScrollView.contentSize = CGSize(width: view.frame.width*CGFloat(slides.count), height: view.frame.height)
         slideScrollView.contentSize = CGSize(width: view.frame.width*CGFloat(slides.count), height: 1.0)
